8cdb6f547bd948f885aa189c5774da7ddd392fec
sagsango <34793802+sagsango@users.noreply.github.com>
2019-09-02 01:47:26 +0530

    Create Intro   .java

---
+++ Intro   .java
@@ -0,0 +1,69 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+class Practice{   //Class name may starts from upper case
+	public static void main(String args[])
+	{
+		
+	    /**
+	     * variable name 
+		 * start with lowercase [ can have int,_ after 1st latter]
+		 * in genral loverCamalCase for variable
+		 * in genral UpperCamalCase for keyword.
+		 **/
+		
+		
+		/**
+		 * Data Type
+		 * Intigers : int [10 digits] , long 
+		 * Double   : double 
+		 * String   : String 
+		 * Char     : char
+		 * bool     : boolean
+		 **/
+		
+		
+		/**
+		 * BigIntegers
+		 * Initialisation : BigIntiger b1= new BigInteger("0");
+	     * gcd            : b1.gcd(b2);
+	     * multiply       : b1.multiply(b2);
+		 * divide    
+		 * b1.pow(b2);
+	     * biginteger1.modInverse(biginteger2); 
+		 * b1.negate();
+		 * b1.sqrt();
+		 * b1.xor(b2);
+		 * public BigInteger modPow(BigInteger exponent, BigInteger m)
+		 * with intigers  : b1.multiply(BigInteger.valueof(10));
+	     * int x   =  b1.intValue();   // value should be in limit of int x
+         * long y   = b1.longValue();  // value should be in limit of long y
+         * String z = b1.toString();
+         * if (A.compareTo(B) < 0)     // compare
+         **/
+         
+         
+		/**
+		 * arrays
+		 * int myarrayname[];            //declaration
+		 * myarrayname = new int [100];  //allocation
+		 * 
+		 * Two dimensional array:
+         * int[][] twoD_arr = new int[10][20];
+         * 
+         * Three dimensional array:
+         * int[][][] threeD_arr = new int[10][20][30];
+		 **/
+		 
+		 
+		 
+		 
+		 
+	
+		
+		
+	}
+}
