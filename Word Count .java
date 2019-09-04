184b0800156f445719e7eb97dee1b07aa7f9736c
sagsango <34793802+sagsango@users.noreply.github.com>
2019-09-04 18:11:24 +0530

    Create Word Count .java

---
+++ Word Count .java
@@ -0,0 +1,29 @@
+/**
+*Input file : https://www.gutenberg.org/files/98/98-0.txt
+*Title: A Tale of Two Cities
+*       A Story of the French Revolution
+*
+*Author: Charles Dickens
+**/
+
+
+/**
+*File Handling 
+*I/O
+*Split string
+**/
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  File 
+	}
+}
