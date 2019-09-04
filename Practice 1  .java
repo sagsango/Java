4dc8762725696eae3caf8da515480299703a3c97
sagsango <34793802+sagsango@users.noreply.github.com>
2019-09-04 07:23:33 +0530

    Update Practice 1  .java

--- Practice 1  .java
+++ Practice 1  .java
@@ -7,6 +7,7 @@ class ForFunctionCalls{
 	/**
 	 * What static and non statc function mean?
 	 * What static ans non static function mean here?
+	 * Static function can be called without their object.
 	 **/
 	int callfunc1()
 	{
@@ -17,6 +18,9 @@ class ForFunctionCalls{
 		return 10;
 	}
 }
+
+//all the function inside this [ calss containg main ] will be static
+//because there will no object of this call for calling those function
 class JavaPractice
 {
 	
@@ -30,14 +34,22 @@ class JavaPractice
 		ForFunctionCalls obj=new ForFunctionCalls();
 		System.out.println(obj.callfunc1());
 		System.out.println(obj.callfunc2());
+		System.out.println("This one have no object:"+ForFunctionCalls.callfunc2());
+		/**
+		 * Will give error becouse have no object
+	         * System.out.println("This one have no object:"+ForFunctionCalls.callfunc1());
+	         **/
 	}
 	
 	
 	/**
 	 * why this must be static?
+	 * Should be because inside the class of main
 	 **/
 	static int myfunc()
 	{
 		return 10;
 	}
+	
+	
 }
