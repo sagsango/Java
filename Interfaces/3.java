0fc75e6b6b2d07d7f0612636afa8ee445e7d1f38
sagsango <34793802+sagsango@users.noreply.github.com>
2019-09-19 00:57:44 +0530

    Create 3.java

---
+++ Interfaces/3.java
@@ -0,0 +1,39 @@
+//works
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+
+interface vehicleone{
+	int  speed=90;
+	public void distance();
+        public void speed();
+
+}
+
+interface vehicletwo{
+	int distance=100;
+        //int  speed=902;
+	public void speed();
+       public void distance();
+}
+
+class Vehicle  implements vehicleone,vehicletwo{
+	public void distance(){
+		int distance=speed*100; 
+		System.out.println("distance travelled is "+distance);
+	}
+	public void speed(){
+		int speed=distance/100;
+	}
+}
+
+class Main{
+	public static void main(String args[]){
+		System.out.println("Vehicle");
+		Vehicle obj=new Vehicle();
+                obj.distance();
+		obj.speed();
+	}
+}
